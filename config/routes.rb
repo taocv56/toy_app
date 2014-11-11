980c58f7f96cc3abc6ad0e5fde57b1f668068ca7
taocv56 <chutao1993@gmail.com>
2014-11-12 01:28:38 +0700

    Update Gemfile.lock for Heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
-ToyApp::Application.routes.draw do
+Rails.application.routes.draw do
+#ToyApp::Application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
-   root 'application#hello'
+    root 'application#Toy'
+
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
