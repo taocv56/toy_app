60357b914ac80d3e67bb0fb633b70c674e6eac5d
taocv56 <chutao1993@gmail.com>
2014-11-12 07:58:11 +0700

    update gemfile

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
-Rails.application.routes.draw do
-#ToyApp::Application.routes.draw do
+ToyApp::Application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
-    root 'application#Toy'
+    root 'application#hello'
 
 
   # Example of regular route:
